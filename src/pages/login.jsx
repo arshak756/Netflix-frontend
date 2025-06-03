09f87cf9a789a121776c177583f38035f6424ac2
arshak756 <mohammedarshak793@gmail.com>
2025-06-04 04:50:16 +0530

    Update login.jsx

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -12,7 +12,7 @@ export default function Login() {
     if (!email || !password) return setError("All fields are required.");
 
     try {
-      const res = await fetch("netflix-backend-production-b24e.up.railway.app, {
+      const res = await fetch("netflix-backend-production-b24e.up.railway.app/api/login, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
